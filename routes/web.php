6092a6c0740ea1bbbcff7dfd4d24fcee8ea2b503
Thaesu Nandy <thaesunandy1020304050@gmail.com>
2023-03-03 17:54:04 +0630

    search function fix in log controller

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::resource('sms', 'App\Http\Controllers\SmsController');
 Route::resource('agreement', 'App\Http\Controllers\AgreementController');
 //系统日志
 Route::resource('log', 'App\Http\Controllers\LogController');
+Route::post('log_search', 'App\Http\Controllers\LogController@log_search')->name('log_search');
 //系统用户列表
 Route::resource('sysusers', 'App\Http\Controllers\SysUsersController');
 Route::get('/sysusers/modifypass/{id}', 'App\Http\Controllers\SysUsersController@modify_pass')->name('sysusers.modifypass'); //修改系统用户的密码
